821670f354e082403fe0ca997b8c153809ed4473
Okonjo Gilbert <okonjogilbert2@gmail.com>
2022-11-29 14:33:08 +0300

    Revert "Styles"

--- src/pages/DetailsPage.jsx
+++ src/pages/DetailsPage.jsx
@@ -1,12 +1,6 @@
 import React from 'react';
 import CountryInfo from '../components/country-info/CountryInfo';
-import MainHeader from '../components/NavBar/MainHeader';
 
-const DetailsPage = () => (
-  <>
-    <MainHeader title="Country Info" />
-    <CountryInfo />
-  </>
-);
+const DetailsPage = () => <CountryInfo />;
 
 export default DetailsPage;
